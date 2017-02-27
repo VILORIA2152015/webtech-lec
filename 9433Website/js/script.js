6fe10c78d16549d1f1eb1f7a41ae565b87804910
aj <aj.gmail.com>
2017-02-27 21:55:45 +0800

    Possible Website Layout
    Website structure includes html, css, javascript. no contents

---
+++ 9433Website/js/script.js
@@ -0,0 +1,19 @@
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+    $("button").click(function(){
+        $("#div1").fadeIn();
+        $("#div2").fadeIn("slow");
+        $("#div3").fadeIn(3000);
+    });    
+});
